d24cb0c1849ccf07c587d157ecc044e1cf93bed7
haha24 <edvardsen.oyvind@gmail.com>
2019-05-13 15:54:20 +0200

    day 14 counts down to midnight, made ui responsive

--- day-14-countdown-timer/main.js
+++ day-14-countdown-timer/main.js
@@ -3,7 +3,7 @@ window.onload = () => {
     const cell2 = document.querySelector('#cell2');
     const cell3 = document.querySelector('#cell3');
 
-    const endDate = new Date(2019, 4, 13, 16);
+    const endDate = getEndDate();
 
     const updateDisplay = () => {
         const now = new Date();
@@ -13,9 +13,9 @@ window.onload = () => {
         const minutes = Math.floor((diff / (1000 * 60)) % 60);
         const hours = Math.floor((diff / (1000 * 60 * 60)));
 
-        cell1.textContent = hours;
-        cell2.textContent = minutes;
-        cell3.textContent = seconds;
+        cell1.textContent = hours < 10 ? '0' + hours : hours;
+        cell2.textContent = minutes < 10 ? '0' + minutes : minutes;
+        cell3.textContent = seconds < 10 ? '0' + seconds : seconds;
     }
 
 
@@ -24,5 +24,10 @@ window.onload = () => {
     }, 1000);
 
     updateDisplay();
+}
+
+const getEndDate = () => {
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
 }
 
